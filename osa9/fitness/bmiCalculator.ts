34bafba40303ed1a1483c764c1ba1b7343f0f0e1
NinaWelch <nwelch1@live.co.uk>
2020-10-23 00:44:38 +0100

    tehtava 9.5

--- osa9/fitness/bmiCalculator.ts
+++ osa9/fitness/bmiCalculator.ts
@@ -21,7 +21,7 @@ const parseBmiArguments = (args: Array<string>): BmiValues => {
     }
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
     const bmi = weight / Math.pow((height / 100), 2);
 
     if (bmi > 25) {
--- osa9/fitness/index.ts
+++ osa9/fitness/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 const app = express();
 
+import { calculateBmi } from './bmiCalculator';
+
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+    res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+    const { height, weight } = req.query;
+
+    if (!height || !weight) {
+        res.status(400).send({ error: 'missing parameters' });
+    }
+
+    if (isNaN(Number(height)) || isNaN(Number(weight))) {
+        res.status(400).send({ error: 'malformatted parameters' });
+    }
+
+    const bmi = calculateBmi(Number(height), Number(weight));
+
+    res.send({ weight, height, bmi });
 });
 
 const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
