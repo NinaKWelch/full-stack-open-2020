b05b28f21dde5aed0c9ce0d6f08415b2472f315f
NinaWelch <nwelch1@live.co.uk>
2020-10-26 04:21:06 +0000

    korjauskoodi

--- osa9/fitness/index.ts
+++ osa9/fitness/index.ts
@@ -14,16 +14,16 @@ app.get('/bmi', (req, res) => {
     const { height, weight } = req.query;
 
     if (!height || !weight) {
-        res.status(400).json({ error: 'missing parameters' });
+        res.status(400).send({ error: 'missing parameters' });
     }
 
     if (isNaN(Number(height)) || isNaN(Number(weight))) {
-        res.status(400).json({ error: 'malformatted parameters' });
+        res.status(400).send({ error: 'malformatted parameters' });
     }
 
     const bmi = calculateBmi(Number(height), Number(weight));
 
-    res.json({ weight, height, bmi });
+    res.send({ weight, height, bmi });
 });
 
 app.post('/exercises', (req, res) => {
@@ -31,22 +31,22 @@ app.post('/exercises', (req, res) => {
     const { daily_exercises, target } = req.body;
 
     if (!target || !daily_exercises ) {
-        res.status(400).json({ error: 'missing parameters' });
+        res.status(400).send({ error: 'missing parameters' });
     }
     
-    if (daily_exercises instanceof Array && daily_exercises.length > 0) {
+    if (Array.isArray(daily_exercises) && daily_exercises.length > 0) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const exercises: Array<any> = daily_exercises.map(Number);
        
         if (isNaN(Number(target)) || exercises.some(isNaN)) {
-            res.status(400).json({ error: 'malformatted parameters' });
+            res.status(400).send({ error: 'malformatted parameters' });
         }
 
         const data = calculateExercises(Number(target), exercises);
 
         res.json(data);
     } else {
-        res.status(400).json({ error: 'malformatted parameters' });
+        res.status(400).send({ error: 'malformatted parameters' });
     }
 });
 
