38e1bda754af61aa3b66c6d4fcc37236ce20899d
NinaWelch <nwelch1@live.co.uk>
2020-11-07 17:28:28 +0000

    tehtava 9.19

--- osa9/patientor_backend/src/types.ts
+++ osa9/patientor_backend/src/types.ts
@@ -1,12 +1,56 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
 
-export interface Diagnose {
+export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
 }
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge?: Discharge;
+}
+
+interface SickLeave  {
+    startDate: string;
+    endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+  
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export enum Gender {
     Other = 'other',
@@ -21,7 +65,7 @@ export interface Patient {
     ssn: string;
     gender: string;
     occupation: string;
-    entries: Entry[];
+    entries: Array<Entry>;
 }
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
